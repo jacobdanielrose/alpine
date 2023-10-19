d47edee7acf6fc8426e2d0ea0fc7358e8b0217c0
Jacob Rose <jacobdanielrose@protonmail.com>
2023-10-19 10:24:53 +0200

    change default colors
    change default colors

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,17 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  alpine: {
+    body: {
+      backgroundColor: {
+        initial: '#eaf4fa',
+        dark: '#111111'
+      },
+
+      color: {
+        initial: '#27272A',
+        dark: '#ffffff'
+      }
+    }
+  }
 })
